2d7b9c46a23570bc2bfb0f83e04a1a3c20cdd686
AdetonaMichael <powerofelectron616@gmail.com>
2023-10-08 23:45:42 +0100

    important commmit

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// Nuclerar 
 Route::get('/', function () {
     return view('welcome');
 });
